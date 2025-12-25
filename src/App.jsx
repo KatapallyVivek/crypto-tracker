0cb393bbc3027eafa9dffe5a94a591138733b328
Vivek Katapally <vivekkatapally03@gmail.com>
2025-12-25 23:37:04 +0530

    chore: update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
 import Home from "./pages/Home";
 import CryptoPrices from "./pages/CryptoPrices";
 import HowToInvest from "./pages/HowToInvest";
-import CoinDetails from "./pages/CoinDetails"; 
+import CoinDetails from "./pages/CoinDetails";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/prices" element={<CryptoPrices />} />
-        <Route path="/invest" element={<HowToInvest />} />
-        <Route path="/coin/:id" element={<CoinDetails />} />
-      </Routes>
+      <Header />
+
+      {/* Padding so content doesn't hide under header */}
+      <div className="pt-28">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/prices" element={<CryptoPrices />} />
+          <Route path="/invest" element={<HowToInvest />} />
+          <Route path="/coin/:id" element={<CoinDetails />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
