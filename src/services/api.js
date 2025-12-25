bd97a4c90e0e3340abc8d9560d064dfbf7327953
Vivek Katapally <vivekkatapally03@gmail.com>
2025-12-25 13:33:06 +0530

    Updated getCoins to support variable perpage and global search

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,20 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.coingecko.com/api/v3";
-export const getCoins = async () => {
+export const getCoins = async (perPage = 10) => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/coins/markets`,
-      {
-        params: {
-          vs_currency: "usd",
-          order: "market_cap_desc",
-          per_page: 10,
-          page: 1,
-          sparkline: false
-        }
+    const response = await axios.get(`${BASE_URL}/coins/markets`, {
+      params: {
+        vs_currency: "usd",
+        order: "market_cap_desc",
+        per_page: perPage,
+        page: 1,
+        sparkline: false
       }
-    );
+    });
     return response.data;
   } catch (error) {
     console.error("Error fetching coins:", error);
     return [];
   }
-}; 
+};
